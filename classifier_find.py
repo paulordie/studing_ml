19186d2749c26b1e4fcb9c485a91136f6bbc53b0
Paulo Correa <paulo.correa@indt.org.br>
2021-03-28 21:48:30 -0400

    using list to check if 0 or 1

--- classifier_find.py
+++ classifier_find.py
@@ -25,8 +25,8 @@ Y = Y_dummies_df.values
 #verifica numa especie de algoritmo base quais são o maiores acerto para chutes
 # acertos = len(Y)
 # erros = -(sum(Y) - len(Y))
-acertos_1 = len(Y[Y == 'sim'])
-acertos_0 = len(Y[Y == 'nao'])
+acertos_1 = list(Y).count('sim')
+acertos_0 = list(Y).count('nao')
 print("total X[Y == 1] %d" % len(X[Y == 1]))
 taxa_correct_base = 100.0 * max(acertos_1, acertos_0) / len(Y)
 print("taxa de acerto base %.2f" % taxa_correct_base)
