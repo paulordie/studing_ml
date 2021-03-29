712889d89627b445d96ec149ace960606c31dad4
Paulo Correa <paulo.correa@indt.org.br>
2021-03-28 21:26:14 -0400

    compare algo base with algo created

--- classifier_find.py
+++ classifier_find.py
@@ -22,16 +22,20 @@ X = X_dummies_df.values
 Y = Y_dummies_df.values
 
 #verifica numa especie de algoritmo base quais são o maiores acerto para chutes
-acertos = len(Y)
-erros = -(sum(Y) - len(Y))
-print(erros)
+# acertos = len(Y)
+# erros = -(sum(Y) - len(Y))
+acertos_1 = sum(Y)
+acertos_0 = len(Y) - acertos_1
+
+taxa_correct_base = 100.0 * max(acertos_1, acertos_0) / len(Y)
+print("taxa de acerto base %.2f" % taxa_correct_base)
 
 percent_train = 0.9
 #print(X)
 size_train = int(percent_train * len(Y))
-print(size_train)
+#print(size_train)
 size_test = int(len(Y) - size_train)
-print(size_test)
+#print(size_test)
 
 train_data = X[:size_train]
 train_mark = Y[:size_train]
@@ -47,7 +51,7 @@ model.fit(train_data, train_mark)
 
 result = model.predict(test_data)
 different = result - test_mark
-print(different)
+#print(different)
 
 corrects = [obj for obj in different if obj == 0]
 
@@ -56,5 +60,6 @@ total_elements = len(test_data)
 
 taxa_correct = 100.0 * total_correct / total_elements
 
-print(taxa_correct)
+print("taxa de acerto do algoritmo %.2f" % taxa_correct)
+#print(taxa_correct)
 print(total_elements)
